33ee7c51265931f91448e4ac55d240c01943601c
Ahmet Ridvan Caglar <arcaglar@gmail.com>
2020-03-10 00:09:31 +0300

    added header component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
   </div>
 </template>
+
+<script>
+import Header from "@/components/header";
+
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
 
 <style lang="scss">
 @import "./assets/stylesheet/style";
---
+++ src/components/header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <div class="header__logo">arcaglar</div>
+    <div class="shopping-cart">
+      <div class="shopping-cart__badge">4</div>
+      <img
+        src="@/assets/image/shopping-basket.svg"
+        class="shopping-cart__icon"
+      />
+      <span>Cart</span>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
