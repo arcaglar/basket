55a0c08bc23c782ae7413a963d32523c61a40a97
Ahmet Ridvan Caglar <arcaglar@gmail.com>
2020-03-12 00:30:58 +0300

    set build env & created server js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var path = require("path");
+var serveStatic = require("serve-static");
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.listen(port);
+console.log("server started " + port);
